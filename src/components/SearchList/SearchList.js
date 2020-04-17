b8fbc3c65b351d677495605e0615d2583e6b9c8b
uojiakux <ugonna.m.ojiaku@gmail.com>
2020-04-16 23:36:16 -0500

    Added search page styling

--- src/components/SearchList/SearchList.js
+++ src/components/SearchList/SearchList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Button from '../Button/Button';
 import { HashRouter as Router, withRouter } from 'react-router-dom';
+import './SearchList.css';
 
 
 class SearchList extends Component {
@@ -20,17 +20,66 @@ class SearchList extends Component {
       <Router>
       <div>
         <div>
-          <p>
-            Search Results
-          </p>
-          <div className="buttonWrapper">
-            {this.props.state.restaurant ?
-              this.props.state.restaurant.map((restaurant) => {
-                return <Button chooseRestaurant={this.chooseRestaurant} key={restaurant.id} restaurant={restaurant}></Button>
-              })
-              :
-              <p>no restaurants</p>
-            }
+          <h2 className="header-text">
+            Search Results for Soul Food Restuarants
+          </h2>
+          <div className="row">
+            <div className="search-list-column-1">
+              <div className="search-restaurant-cards">
+                <br/>
+                <h3>Sechuan Lilypad</h3>
+                <p className="address-one">1321 Henry Street</p>
+                <p className="address-two">Roseville, MN 56575</p>
+                <p className="phone">651-278-9829</p>
+              </div>
+              <div className="search-restaurant-cards">
+                <br/>
+                <h3>Lotus Flower</h3>
+                <p className="address-one">1321 Cherry Blossom Rd</p>
+                <p className="address-two">New Brighton, MN 87899</p>
+                <p className="phone">652-987-3847</p>
+              </div>
+              <div className="search-restaurant-cards">
+                <br/>
+                <h3>Old Shanghai</h3>
+                <p className="address-one">1321 Cherry Blossom Rd</p>
+                <p className="address-two">New Brighton, MN 87899</p>
+                <p className="phone">652-987-3847</p>
+              </div>
+              <div className="search-restaurant-cards">
+                <br/>
+                <h3>Demera</h3>
+                <p className="address-one">1321 Cherry Blossom Rd</p>
+                <p className="address-two">New Brighton, MN 87899</p>
+                <p className="phone">652-987-3847</p>
+              </div>
+              <div className="search-restaurant-cards">
+                <br/>
+                <h3>Old Chicago</h3>
+                <p className="address-one">1321 Cherry Blossom Rd</p>
+                <p className="address-two">New Brighton, MN 87899</p>
+                <p className="phone">652-987-3847</p>
+              </div>
+            </div>
+
+            <div className="search-list-column-2">
+              <div className="search-restaurant-card-result">
+                  <br/>
+                  <img src="https://www.revivalrestaurants.com/img/revival-minneapolis.jpg" alt="Restaurant food image" className="featured-restaurant-img"></img>
+                  <h3>Revival</h3>
+                  <p className="address-one">1321 Henry Street</p>
+                  <p className="address-two">Roseville, MN 56575</p>
+                  <p className="phone">651-278-9829</p>
+                  <br/>
+                  <bold>
+                  <a href="#" className="link-text">View menu and hours</a>
+                  </bold>
+                </div>
+            </div>
+          </div>
+
+          <div>
+
           </div>
         </div>
       </div>
