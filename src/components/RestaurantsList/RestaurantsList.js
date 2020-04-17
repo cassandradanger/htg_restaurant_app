e6a679e066b8aea74c4c33b036f98c4243b8e995
Misky Abshir <Misky.Abshir@gmail.com>
2020-04-17 15:35:16 -0500

    made simple changes

--- src/components/RestaurantsList/RestaurantsList.js
+++ src/components/RestaurantsList/RestaurantsList.js
@@ -1,41 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Button from '../Button/Button';
-
+import Restaurant from '../restaurant/restaurant'
 import './RestaurantsList.css'
 
 class RestaurantList extends Component {
 
-  componentDidMount () {
-    this.props.dispatch({type: 'FETCH_RESTAURANTS'});
-  }
-
-  chooseRestaurant = (event, restaurant) => {
-    console.log(restaurant);
-    this.props.dispatch({type: 'FETCH_CHOSEN_RESTAURANT', payload: restaurant});
-    this.props.history.push('/restaurantView')
-  }
+  
   render() {
     return (
-        <div>
-      <p>
-            Here is a list of restaraunts 
-          </p>
-   
-          <div>
-            {this.props.state.restaurant ?
-              <div class="flex-container" onClick={this.chooseRestaurant}>{this.props.state.restaurant.map((restaurant) => 
-                <div key={restaurant.id}>
-                  <p>{restaurant.name}</p>
-                  <img src={restaurant.picture} />
-                </div>
-              )}
-              </div>
-              :
-              <p>no restaurants</p>
-            }
-          </div>
-</div>
+        <div className="Container">
+            <center>
+            <p>Here is a list of restaraunts </p>
+            </center>
+            <div className="flex-container">
+            <Restaurant />
+            </div>
+      
+    </div>
     )
   }
 }
