e22ca0bf2fd8b5f9897e4173de3647241e0183f2
Casie Siekman <casie.siekman@gmail.com>
2020-04-16 21:36:17 -0500

    styling

--- src/components/AddRestaurant/AddRestaurant.js
+++ src/components/AddRestaurant/AddRestaurant.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Button from '../Button/Button';
+import './AddRestaurant.css';
+
 
 class AddRestaurant extends Component {
   state = {
@@ -9,7 +11,7 @@ class AddRestaurant extends Component {
     address: '',
     city: '',
     state: '',
-    zipcode: 0,
+    zipcode: '',
     hours: '',
     link: '',
     welcomeNote: '',
@@ -55,22 +57,18 @@ class AddRestaurant extends Component {
 
   render() {
     return(
-      <div>
+      <div className="formWrapper">
+          <p>Hi!
+            Thank you for adding your restaurant to our database. 
+            Our goal is to help diners find restaurants and order dishes easily. 
+            We are asking you to provide accurate information. 
+            Please make sure to update the information on your restaurant as soon as there are changes. 
+            Thank you. 
+          </p>
         <form onSubmit={this.addNewRestaurant}>
           <h1>Add a restaurant</h1>
           <div>
-            <label htmlFor="name">
-              Name:
-              <input
-                type="text"
-                name="name"
-                value={this.state.name}
-                onChange={this.handleInputChangeFor('name')}
-              />
-            </label>
-          </div>
-          <div>
-          <label htmlFor="type">Food Type:</label>
+          <label htmlFor="type">Restaurant Cuisine Type*</label>
             <select id="type" name="type">
               <option value="American">American</option>
               <option value="Italian">Italian</option>
@@ -79,156 +77,169 @@ class AddRestaurant extends Component {
             </select>
           </div>
           <div>
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleInputChangeFor('name')}
+            />
+            <label htmlFor="name">
+              Restaurant Name*
+            </label>
+          </div>
+          <div>
+            <input
+              type="text"
+              name="phone"
+              value={this.state.phone}
+              onChange={this.handleInputChangeFor('phone')}
+            />
             <label htmlFor="username">
-              Phone Number:
-              <input
-                type="text"
-                name="phone"
-                value={this.state.phone}
-                onChange={this.handleInputChangeFor('phone')}
-              />
+              Phone Number*
             </label>
           </div>
           <div>
+            <input
+              type="text"
+              name="address"
+              value={this.state.address}
+              onChange={this.handleInputChangeFor('address')}
+            />
             <label htmlFor="address">
-             Street Address:
-              <input
-                type="text"
-                name="address"
-                value={this.state.address}
-                onChange={this.handleInputChangeFor('address')}
-              />
+             Street Address
             </label>
           </div>
           <div>
+            <input
+              type="text"
+              name="city"
+              value={this.state.city}
+              onChange={this.handleInputChangeFor('city')}
+            />
             <label htmlFor="city">
-             City:
-              <input
-                type="text"
-                name="city"
-                value={this.state.city}
-                onChange={this.handleInputChangeFor('city')}
-              />
+             City
             </label>
           </div>
           <div>
+            <input
+              type="text"
+              name="state"
+              value={this.state.state}
+              onChange={this.handleInputChangeFor('state')}
+            />
             <label htmlFor="state">
-             State:
-              <input
-                type="text"
-                name="state"
-                value={this.state.state}
-                onChange={this.handleInputChangeFor('state')}
-              />
+             State
             </label>
           </div>
           <div>
+            <input
+              type="integer"
+              name="zipcode"
+              value={this.state.zipcode}
+              onChange={this.handleInputChangeFor('zipcode')}
+            />
             <label htmlFor="zipcode">
-             Zipcode:
-              <input
-                type="integer"
-                name="zipcode"
-                value={this.state.zipcode}
-                onChange={this.handleInputChangeFor('zipcode')}
-              />
+             Zipcode
             </label>
           </div>
           <div>
+            <input
+              type="text"
+              name="hours"
+              value={this.state.hours}
+              onChange={this.handleInputChangeFor('hours')}
+            />
             <label htmlFor="hours">
-             Hours of Operation:
-              <input
-                type="text"
-                name="hours"
-                value={this.state.hours}
-                onChange={this.handleInputChangeFor('hours')}
-              />
+             Hours of Operation
             </label>
           </div>
           <div>
+            <input
+              type="text"
+              name="link"
+              value={this.state.link}
+              onChange={this.handleInputChangeFor('link')}
+            />
             <label htmlFor="link">
-             Link:
-              <input
-                type="text"
-                name="link"
-                value={this.state.link}
-                onChange={this.handleInputChangeFor('link')}
-              />
+             Website URL
             </label>
           </div>
           <div>
-            <label htmlFor="welcomeNote">
-             Welcome Note for Customers:
-              <input
-                type="text"
-                name="welcomeNote"
-                value={this.state.welcomeNote}
-                onChange={this.handleInputChangeFor('welcomeNote')}
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="description">
-             Restaurant Description:
-              <textarea
-                name="description"
-                value={this.state.description}
-                onChange={this.handleInputChangeFor('description')}
-              />
-            </label>
-          </div>
-          <div>
+            <input
+              type="text"
+              name="picture"
+              value={this.state.picture}
+              onChange={this.handleInputChangeFor('picture')}
+            />
             <label htmlFor="picture">
-             Picture URL:
-              <input
-                type="text"
-                name="picture"
-                value={this.state.picture}
-                onChange={this.handleInputChangeFor('picture')}
-              />
+             Main Image URL
             </label>
           </div>
           <div>
-            <label htmlFor="delivery">
-             Delivery Available:
+            <input
+              type="text"
+              name="welcomeNote"
+              value={this.state.welcomeNote}
+              onChange={this.handleInputChangeFor('welcomeNote')}
+            />
+            <label htmlFor="welcomeNote">
+             Welcome Note for Customers
+            </label>
+          </div>
+          <div>
+            <textarea
+              name="description"
+              value={this.state.description}
+              onChange={this.handleInputChangeFor('description')}
+            />
+            <label htmlFor="description">
+             Restaurant Description
+            </label>
+          </div>
+          <div className="radioWrapper">
+            <div>
+              <label htmlFor="delivery">
+              Delivery Available 
+              </label>
               <input
                 type="radio"
                 name="delivery"
                 value='true'
                 onChange={this.handleInputChangeFor('delivery')}
               />
-            </label>
-            <label htmlFor="delivery">
-            No Delivery:
+              <label htmlFor="delivery">
+                No Delivery 
+              </label>
               <input
                 type="radio"
                 name="delivery"
                 value='false'
                 onChange={this.handleInputChangeFor('delivery')}
               />
-            </label>
+            </div>
           </div>
-          <div>
-            <label htmlFor="pickUp">
-             Pick Up Available:
+          <div className="radioWrapper">
+            <div>
+              <label htmlFor="pickUp">
+              Pick Up Available 
+              </label>
               <input
                 type="radio"
                 name="pickUp"
                 value='true'
                 onChange={this.handleInputChangeFor('pickUp')}
               />
-            </label>
-            <label htmlFor="pickUp">
-             No Pick Up:
+              <label htmlFor="pickUp">
+              No Pick Up 
+              </label>
               <input
                 type="radio"
                 name="pickUp"
                 value='false'
                 onChange={this.handleInputChangeFor('pickUp')}
               />
-            </label>
+            </div>
           </div>
-
-
 
           <div>
             <input
